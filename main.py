20554a2399c7872d4d519f7834acb410337c13b3
Tahmin Ahmed <otamam818@gmail.com>
2021-08-16 17:43:30 +0600

    Fixed logic in __get_text_sets() method in BOW class

--- main.py
+++ main.py
@@ -10,11 +10,39 @@ class BOW:
         \ntexts: an iterable of strings. Each string carries a set of words/sentences"""
         # The originally input text file
         self.original = texts
-        self.__text_list = []
-        for i in texts:
-            self.__text_list.append(set(word_tokenize(i)))
+        
         # set of all accepted stopwords
         self.stopwords = set(stopwords.words('english'))
+        
+        # list of set of tokenized words filtered from stopwords
+        self.__text_list = self.__get_text_sets()
+        
+        # useless words found throughout the text and their count
+        self.useless = dict()
+        
+        # a list of dictionaries. Based on how many times each 
+        # word repeated in given text body (based on index)
+        self.__repeats_list = [] 
+
+    def __get_text_sets(self):
+        finlist = []
+        texts = self.original
+        for i in range(len(texts)):
+            # make a set for each text and add 
+            # their words (if it's not a stopword)
+            finlist.append(set())
+            for word in texts[i]:
+                if not(word in self.stopwords):
+                    finlist[i].add(word)
+                
+        return [set(word_tokenize(i)) for i in texts if not(i in self.stopwords)]
+
+    def __get_repeats(self):
+        # make a dictionary that counts the 
+        finlist = []
+        for i in self.original:
+            finlist.append(dict())
+
 
 
 
