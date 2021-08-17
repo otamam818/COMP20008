735d57630b3205f59e2e0ae004e064aff78fc7fd
Tahmin Ahmed <otamam818@gmail.com>
2021-08-17 12:06:06 +0600

    Tested methods and added fixes

--- BagOfWords/head.py
+++ BagOfWords/head.py
@@ -1,8 +1,13 @@
 # NLTK library
 # for getting all accepted stopwords
 from nltk.corpus import stopwords
+#adding custom stopwords
+my_stopwords = set(stopwords.words('english'))
+my_stopwords.add("n't")
+
 # for getting a tokenizer function
 from nltk.tokenize import word_tokenize
+from re import split
 
 # Pandas - imported to create the appropriate dataframe
 import pandas as pd
--- BagOfWords/main.py
+++ BagOfWords/main.py
@@ -12,14 +12,15 @@ class BOW:
         # The originally input text file
         self.original = texts
         
-        # set of all accepted stopwords
-        self.stopwords = set(stopwords.words('english'))
-        
-        # list of set of tokenized words filtered from stopwords
-        self.__text_list = self.__get_text_sets()
+        # set of all accepted stopwords + extras
+        self.stopwords = my_stopwords
         
         # useless words found throughout the text and their count
         self.useless = dict()
+
+        # list of set of tokenized words filtered from stopwords
+        self.__text_list = self.__get_text_sets()
+        
         
         # a list of dictionaries. Based on how many times each 
         # word repeated in given text body (based on index)
@@ -33,10 +34,18 @@ class BOW:
             # make a set for each text and add 
             # their words (if it's not a stopword)
             finlist.append(set())
-            for word in texts[i]:
-                if not(word in self.stopwords):
-                    finlist[i].add(word)
+            for word in word_tokenize(texts[i]):
+                word = "".join(split("'\w*", word.casefold()))
+                valid_word = not(word in self.stopwords)
+                if valid_word:
+                    finlist[i].add(word.casefold())
+                else:
+                    if word in self.useless:
+                        self.useless[word] += 1
+                    else:
+                        self.useless[word] = 1
                 
+        print(f"finlist is {finlist}")
         return finlist
 
     def __get_repeats(self) -> list:
@@ -46,8 +55,10 @@ class BOW:
         tokenized_wordset = self.__text_list
         for i in range(len(self.original)):
             finlist.append(dict())
-            for word in self.original[i]:
-                self.__append_accordingly(finlist, tokenized_wordset, i, word)
+            print(f"Now working on: {word_tokenize(self.original[i])}\n\n")
+            for word in word_tokenize(self.original[i]):
+                self.__append_accordingly(finlist, tokenized_wordset, 
+                                          i, word.casefold())
         return finlist
 
     @staticmethod
@@ -64,7 +75,20 @@ class BOW:
 
 def test():
     """Method used for testing implemented class"""
-    pass  
+    texts = ["Gaara of the Sand didn't die", "Naruto - Gaara's best friend"]
+    # print("Enter text then press ENTER to enter another text")
+    # print("Press CTRL+C to exit once you are done pressing ENTER")
+    try:
+        raise KeyboardInterrupt
+        # while True:
+        #     texts.append(input())
+    except KeyboardInterrupt:
+        bow1 = BOW(texts)
+        print(f"\nOriginal text:\n{bow1.original}"               + '\n')
+        print(f"\nStopwords:\n{bow1.stopwords}"                  + '\n')
+        print(f"\nUseless words found:\n{bow1.useless}"          + '\n')
+        print(f"\nFirst text  (tokenized): {bow1.get_repeats(0)}" + '\n')
+        print(f"\nSecond text (tokenized): {bow1.get_repeats(1)}" + '\n')
 
 if __name__ == "__main__":
     test()
